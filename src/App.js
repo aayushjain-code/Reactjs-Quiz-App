499fbab13481af1e8db886b95abc6488a72e783f
Aayush Jain <75216351+Aayushjain-code@users.noreply.github.com>
2021-05-08 23:42:03 +0530

    created file structure and header component done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header/Header'
 
 function App() {
   return (
-    <div className="App">
-      header
-    </div>
+
+    <BrowserRouter>
+      <div className="app" style={{ backgroundImage: 'url("/ques1.png")' }}>
+        <Header />
+
+      </div >
+
+    </BrowserRouter>
+
   );
 }
 
