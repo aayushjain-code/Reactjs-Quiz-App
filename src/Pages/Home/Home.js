f09b8be395c1176de0444d1de214d5f54e85127e
Aayush Jain <75216351+Aayushjain-code@users.noreply.github.com>
2021-05-09 17:27:55 +0530

    setting up states

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -2,20 +2,32 @@ import React from 'react'
 import "./Home.css";
 import { Button, MenuItem, TextField } from "@material-ui/core"
 import Categories from "../../Data/Categories";
+import { useState } from 'react';
 
 
-const Home = () => {
+const Home = ({ name, setName }) => {
+	const [category, setCategory] = useState("");
+	const [difficulty, setDifficulty] = useState("");
+
+
+
+
+
+
 	return (
 		<div className="content">
 			<div>
 				<span style={{ fontSize: 30 }}>Quiz Settings</span>
 				<div className="settings__select">
-					<TextField style={{ marginBottom: 25 }} label="Enter Your Name" Variant="outlined" />
+					<TextField
+						style={{ marginBottom: 25 }} label="Enter Your Name" Variant="outlined"
+						onChange={(e) => setName(e.target.value)}
+					/>
 					<TextField
 						select
 						label="Select Category"
-
-
+						value={category}
+						onChange={(e) => setCategory(e.target.value)}
 						variant="outlined"
 						style={{ marginBottom: 30 }}
 					>
@@ -28,6 +40,8 @@ const Home = () => {
 					<TextField
 						select
 						label="Select Difficulty"
+						value={difficulty}
+						onChange={(e) => setDifficulty(e.target.value)}
 						variant="outlined"
 						style={{ marginBottom: 30 }}
 					>
